5e2b52f7e49fc68248e220008fcef8bf62dbbac4
reyvdb <reyvdb@gmail.com>
2023-02-27 20:25:50 +0200

    add first 6 solutions for easy_1

--- js101/js101-js119_small_problems/easy_1/easy_1.js
+++ js101/js101-js119_small_problems/easy_1/easy_1.js
@@ -87,7 +87,274 @@
 
 
 
+
 // Log all even numbers from 1 to 99, inclusive, to the console, with each number on a separate line.
-for (let i = 2; i <= 99; i+=2) {
-  console.log(i);
-}
+// for (let i = 2; i <= 99; i+=2) {
+//   console.log(i);
+// }
+
+
+
+
+// How big is the room?
+// Build a program that asks the user to enter the length and width of a room in meters, 
+// and then logs the area of the room to the console in both square meters and square feet.
+
+// Note: 1 square meter == 10.7639 square feet
+
+// Do not worry about validating the input at this time. 
+// Use the readlineSync.prompt method to collect user input.
+
+// Example:
+
+// Enter the length of the room in meters:
+// 10
+// Enter the width of the room in meters:
+// 7
+// The area of the room is 70.00 square meters (753.47 square feet).
+
+// P
+// Get user input for length and width
+// Multiply the two 
+// Log an answer in metres and in feet
+
+// E
+// Edge cases:
+// Letter inputs AND 0 number and NaN/Undefined inputs
+
+// D & A
+// 1. Assign first user input to variable called length after checking validity
+// 2. Assign second user input to variable called width after checking validity
+// 3. Multiply two variables together and assign to variable called meters
+// 4. Multiply meters variable by 10.7639 and assign result to variabled called feet
+// 5. Log both results
+
+// C
+// const readlineSync = require('readline-sync');
+// const ONE_METER_IN_FEET = 10.7639;
+
+// function isNotValid(num) {
+//   return num.trim() === '' ||
+//           Number(num) === 0 ||
+//           Number(num) === Infinity ||
+//           Number(num) === -Infinity ||
+//           Number.isNaN(Number(num));
+// }
+
+// function calculateRoomSize() {
+//   console.log("Enter the length of the room in meters");
+//   let length = readlineSync.question();
+  
+//   while (isNotValid(length)) {
+//     console.log("That is invalid. Please try again.");
+//     length = readlineSync.question();
+//   }
+  
+//   console.log("Enter the width of the room in meters");
+//   let width = readlineSync.question();
+  
+//   while (isNotValid(width)) {
+//     console.log("That is invalid. Please try again.");
+//     width = readlineSync.question();
+//   }
+  
+//   let sizeInMeters = (length * width);
+//   let sizeInFeet = (sizeInMeters * ONE_METER_IN_FEET);
+
+//   return { sizeInMeters, sizeInFeet };
+// }
+
+// function displayRoomSize(meters, feet) {
+//   console.log(`The area of the room is ${meters.toFixed(2)} square meters (${feet.toFixed(2)} square feet).`);
+// }
+
+// let { sizeInMeters, sizeInFeet } = calculateRoomSize();
+// displayRoomSize(sizeInMeters, sizeInFeet);
+
+
+
+
+// Tip Calculator
+// Create a simple tip calculator. 
+// The program should prompt for a bill amount and a tip rate. 
+// The program must compute the tip, and then log both the tip and the total amount of the bill to the console. 
+// You can ignore input validation and assume that the user will enter numbers.
+
+// Example:
+
+// What is the bill? 200
+// What is the tip percentage? 15
+
+// The tip is $30.00
+// The total is $230.00
+
+// P
+// Number input is assumed to always be valid
+// Calculation of the total amount after a tip has been added by user
+// Log both the tip amount and the total bill
+
+// E
+// Bill of $0 should always log 0 for tipTotal and totalBill
+// Tip of 0% should result in tipTotal of 0
+
+// D & A
+// Get user input for the bill amount and set it to a variabled called "billAmount"
+// Get user input for the tip percent and set it to a variable called "tipPercent"
+// Multiply tipPercent by bill and set to tipTotal 
+// Sum tipTotal and billAmount and set to totalBill
+// Log tipTotal and totalBill
+
+// const readline = require('readline-sync');
+
+// console.log("What is the bill?");
+// let billAmount = parseFloat(readline.question());
+
+// console.log("What is the tip percentage?");
+// let tipPercent = parseFloat(readline.question());
+
+// let tipTotal = tipPercent / billAmount;
+// let billTotal = tipTotal + billAmount;
+
+// console.log(`The tip is ${tipTotal}`);
+// console.log(`The total is ${billTotal}`);
+
+
+
+
+// Write a program that asks the user to enter an integer greater than 0, 
+// then asks whether the user wants to determine the sum or the product of all numbers 
+// between 1 and the entered integer, inclusive.
+
+// Examples:
+
+// Please enter an integer greater than 0: 5
+// Enter "s" to compute the sum, or "p" to compute the product. s
+
+// The sum of the integers between 1 and 5 is 15.
+// Please enter an integer greater than 0: 6
+// Enter "s" to compute the sum, or "p" to compute the product. p
+
+// The product of the integers between 1 and 6 is 720.
+
+// P
+// inputs
+//  an integer greater than 0
+// "s" or "p"
+// outputs
+//  the sum or the product of all numbers between 1 and the entered integer (inclusive)
+//
+// inputs
+//  5
+//  "s"
+// outputs
+//  15
+
+// E
+// 1
+// inputs
+//  10
+//  "p"
+// output
+//  3628800
+// 2
+// inputs
+// 5
+// 's'
+// output
+// 15
+
+// D & A
+// Get integer from user and set to variable "number"
+// Get "s" or "p" from user and set to computeOption
+// if "s"
+//  initialize "total" variable
+//  loop from 1 up to and including "number"
+//    add the value of the iterator to "total"
+//    increment iterator by 1
+// if "p"
+//  initialize "total" variable and set to 1
+//  loop from 1 up to and including "number"
+//    multiply iterator by "total" and "total" to result
+//    increment iterator by 1
+
+// C
+// const readline = require('readline-sync');
+
+// function computeSum() {
+//   let total = 0;
+
+//   if (computeChoice === 's') {
+//     for (let i = 1; i <= number; i++) {
+//       total += i;
+//     }
+//   }
+
+//   return total;
+// }
+
+// function computeProduct() {
+//   let total = 1;
+
+//   if (computeChoice === 'p') {
+//     for (let i = 1; i <= number; i++) {
+//       total *= i
+//     }
+//   }
+  
+//   return total
+// }
+
+// console.log("Please enter an integer greater than 0:")
+// let number = readline.question();
+
+// console.log("Enter 's' to compute the sum, or 'p' to compute the product.");
+// let computeChoice = readline.question();
+
+// if (computeChoice === 's') {
+//   let sum = computeSum();
+//   console.log(`The product of the integers between 1 and ${number} is ${sum}.`)
+// } else if (computeChoice === 'p') {
+//   let product = computeProduct();
+//   console.log(`The sum of the integers between 1 and ${number} is ${product}.`)
+// } else {
+//   console.log("Invalid operation")
+// }
+
+// Further Exploration
+// What if the input was an array of integers instead of just a single integer? 
+// How would your computeSum and computeProduct functions change? 
+// Given that the input is an array, how might you make use of the Array.prototype.reduce() method?
+
+// const readline = require('readline-sync');
+
+// function computeSum(arr) {
+//   return arr.reduce((sum, num) => sum += parseInt(num), 0)
+// }
+
+// function computeProduct(arr) {
+//   return arr.reduce((product, num) => product *= parseInt(num), 1)
+// }
+
+// function cleanInputArray(inputArray) {
+//   return inputArray.split(',')
+//             .filter(val => !Number.isNaN(parseInt(val)))
+//             .map(val => parseInt(val));
+// }
+
+// console.log("Please enter an array of numbers:")
+// let input = readline.question();
+
+// let cleanedInputArray = cleanInputArray(input);
+
+// console.log("Enter 's' to compute the sum, or 'p' to compute the product.");
+// let computeChoice = readline.question();
+
+// if (computeChoice === 's') {
+//   let sum = computeSum(cleanedInputArray);
+//   console.log(`The sum of the integers in the array [${cleanedInputArray}] is ${sum}.`)
+// } else if (computeChoice === 'p') {
+//   let product = computeProduct(cleanedInputArray);
+//   console.log(`The product of the integers between [${cleanedInputArray}] is ${product}.`)
+// } else {
+//   console.log("Invalid operation")
+// }
