c7acc809c357a410022b7907e82a43b18c4edef6
reyvdb <reyvdb@gmail.com>
2024-06-11 09:02:26 +0900

    add js101 lesson 3 hard practice problems

---
+++ js101/js101_lesson_3_practice_problems/hard_1/hard_1.js
@@ -0,0 +1,31 @@
+function isDotSeparatedIpAddress(inputString) {
+  let dotSeparatedWords = inputString.split(".");
+
+  if (dotSeparatedWords.length !== 4) {
+    return false;
+  }
+
+  while (dotSeparatedWords.length > 0) {
+    let word = dotSeparatedWords.pop();
+    if (!isAnIpNumber(word)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function isAnIpNumber(str) {
+  if (/^\d+$/.test(str)) {
+    let number = Number(str);
+    return number >= 0 && number <= 255;
+  }
+
+  return false;
+}
+
+console.log(isDotSeparatedIpAddress('10.20.43.42'));
+console.log(isDotSeparatedIpAddress('10.20.43'));
+console.log(isDotSeparatedIpAddress('10.20.43.20.74'));
+console.log(isDotSeparatedIpAddress(''));
+console.log(isDotSeparatedIpAddress('hello'));
