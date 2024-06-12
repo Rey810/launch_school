8cfd9601ca52dde1b53cc9bb70969b3579946632
reyvdb <reyvdb@gmail.com>
2024-06-12 21:07:57 +0900

    add deck creation and shuffle

--- js110/lesson_3/twenty_one.js
+++ js110/lesson_3/twenty_one.js
@@ -1,3 +1,38 @@
+// initialise and shuffle card deck
+function initialiseDeck() {
+  let sortedDeck = [];
+
+  // create card deck
+  for (let index = 0; index <= 12; index++) {
+    // create set of cards for 4 suites: clubs, spades, hearts, diamonds
+    const suites = ['C', 'S', 'H', 'D'];
+    const cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+
+    // add suite/card subarrays to array
+    suites.forEach(suite => {
+      sortedDeck.push([suite, cards[index]])
+    })
+  }
+
+  return sortedDeck;
+}
+
+function shuffle(arr) {
+  let array = [...arr]; 
+
+  for (let index = array.length - 1; index > 0; index--) {
+    let otherIndex = Math.floor(Math.random() * (index + 1));
+    [array[otherIndex], array[index]] = [array[index], array[otherIndex]];
+  }
+
+  return array;
+}
+
+let sortedDeck = initialiseDeck();
+let shuffledDeck = shuffle(sortedDeck);
+
+console.log(shuffledDeck);
+
 /* data structure 
 deck: {
   spades: [
@@ -81,3 +116,26 @@ dealer: [
 // FUNCTION FOR RESULT OF GAME
 
 // FUNCTION FOR DISPLAYING GAME RESULT
+
+
+// ALGORITHM
+// create data structures for the deck, player cards and dealer cards
+// start gameloop
+//    shuffle deck
+//    give player two cards, give dealer two cards
+//    playerturn
+//       - player hits or stays
+//         - continues until player busts or stays
+//           - if busts, dealer wins
+//             - display that dealer is the winner
+//           - if stays, dealerturn unless dealer has stayed (in which case compare cards)
+//    dealerturn
+//       - dealer hits until stay (when total is at least 17 but less than 21) or bust
+//         - if stay, back to player
+//         - if bust, player wins
+//           - display that player is the winner
+//    ask user if they want to play again, 
+//        - if yes, shuffle cards and rerun game loop 
+//        - if no, say bye and quit
+
+
