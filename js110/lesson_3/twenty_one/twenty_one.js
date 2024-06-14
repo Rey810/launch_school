d5906605be1c708f0c77d15989762de79a7f8fd1
reyvdb <reyvdb@gmail.com>
2024-06-14 20:18:35 +0900

    add initial working solution

--- js110/lesson_3/twenty_one/twenty_one.js
+++ js110/lesson_3/twenty_one/twenty_one.js
@@ -14,29 +14,23 @@ function capitalized(word) {
 
 const GAME_STATE = {
   players: {
-    player1:
-    {
+    player: {
       name: 'player',
       hand: [],
       stay: false,
       bust: false,
+      score: 0
     },
-    player2: 
-    {
+    dealer: {
       name: 'dealer',
       hand: [],
       stay: false,
-      bust: false
-    },
+      bust: false,
+      score: 0,
+    }
   },
   winner: null
 }
-
-let sortedDeck = initialiseDeck();
-let shuffledDeck = shuffle(sortedDeck);
-initialiseCardHands(GAME_STATE.players.player1.hand, GAME_STATE.players.player2.hand);
-playerTurn(GAME_STATE.players.player1)
-
 
 // initialise and shuffle card deck
 function initialiseDeck() {
@@ -70,44 +64,86 @@ function shuffle(arr) {
 }
 
 // give player two cards, give dealer two cards
-function initialiseCardHands(player1, player2) {
+function initialiseCardHands(player, dealer) {
   // give a random card to each player twice
   for (let count = 1; count < 3; count++) {
     // deck is already shuffled so just pop a card off the top
-    player1.push(shuffledDeck.pop())
-    player2.push(shuffledDeck.pop())
+    player.push(shuffledDeck.pop())
+    dealer.push(shuffledDeck.pop())
   }
 }
 
-// playerturn
-//       - player hits or stays
-//         - continues until player busts or stays
-//           - if busts, dealer wins
-//             - display that dealer is the winner
-//           - if stays, dealerturn unless dealer has stayed (in which case compare cards)
+
 function playerTurn(player){
+  console.log(player);
+  printMessage(`Your current hand is as follows: ${JSON.stringify(player.hand)}`);
 
   while (!player.stay && !player.bust) {
     let { hand } = player
 
-    if (hitOrStay() === 'hit') {
+    let hitOrStayResult = hitOrStay();
+    console.log('hitOrStay', hitOrStayResult === 'stay')
+
+    if (hitOrStayResult === 'hit') {
       // give another card
       dealCard(hand);
+      printMessage(`Your hand now has ${hand.length} cards: ${JSON.stringify(player.hand)}`);
+
       // check for bust
       if (isBust(hand)) {
         player.bust = true;
         setGameResult(player.name);
-        displayWinner();
+        displayResult();
       }
-
-      console.log(GAME_STATE);
-      // hitOrStay
     };
+
+    if (hitOrStayResult === 'stay') {
+      console.log("inside hitOrStay === stay", player)
+      player.stay = true;
+      // if stays, dealerturn unless dealer has stayed (in which case compare cards)
+      GAME_STATE.players.dealer.stay = false ? dealerTurn(GAME_STATE.players.dealer) : compareCards(GAME_STATE.players.player, GAME_STATE.players.dealer);
+    }
   }
+}
 
+function dealerTurn(dealer) {
+  let { hand } = dealer;
+  dealer.score = sumCards(dealer.hand);
 
-  // if (bust) return displayWinner(dealer);
-  // if (stay) dealerTurn(DEALER_HAND);
+  while(!dealer.stay && !dealer.bust) {
+    if ((dealer.score > 17) && (dealer.score <= 21)) {
+      dealer.stay = true;
+      compareCards(GAME_STATE.players.player, GAME_STATE.players.dealer);
+    } else {
+      dealCard(hand);
+
+      // check for bust
+      if (isBust(hand)) {
+        player.bust = true;
+        setGameResult(dealer.name);
+        displayResult();
+      }
+    }
+  }
+}
+
+function compareCards(player, dealer) {
+  player.score = sumCards(player.hand);
+  dealer.score = sumCards(dealer.hand);
+
+  console.log('scores', `Player Score: ${player.score}, Dealer Score: ${dealer.score}`);
+
+  if (player.score > dealer.score) {
+    // dealer is the loser
+    setGameResult(dealer.name);
+    displayResult();
+    } else if (player.score < dealer.score) {
+      setGameResult(player.name);
+    displayResult();
+  } else {
+    // in the event of a tie
+    displayResult();
+  }
 }
 
 function hitOrStay(){
@@ -115,26 +151,23 @@ function hitOrStay(){
   // get user input
   userChoice = readline.question().slice(0, 1).toLowerCase();
 
-  console.log(userChoice);
-  // check input
-  // loop until input is correct
+  // check input on a loop until input is correct
   if (!['s', 'h'].includes(userChoice)) {
     printMessage(messages.notValidInput);
     userChoice = readline.question().slice(0, 1).toLowerCase();
   }
 
   return userChoice === 'h' ? 'hit' : 'stay';
-
-  // if stay, player.stay = true
-  // if hit, deal card, check for bust, hitOrStay again
 }
 
 function dealCard(hand) {
   return hand.push(shuffledDeck.pop());
 }
 
-function isBust(hand) {
+function sumCards(hand) {
   let sum = 0;
+
+  console.log('hand', hand)
   
   hand.forEach(card => {
     if (['J', 'Q', 'K'].includes(card[1])) {
@@ -146,109 +179,32 @@ function isBust(hand) {
     }
   });
 
-  console.log('isBust score', sum);
+  return sum;
+}
 
-  return sum > 21;
+function isBust(hand) {
+  return sumCards(hand) > 21
 }
 
 function setGameResult(loserName) {
   let winner;
+
   for (let player in GAME_STATE.players) {
-    if (player.name !== loserName) winner = GAME_STATE.players[player];
+    if (player !== loserName) winner = GAME_STATE.players[player];
   }
 
   GAME_STATE.winner = winner;
-  console.log('inside set game result', { GAME_STATE })
-}
-
-function displayWinner() {
-  let winner = GAME_STATE.winner.name;
-
-  printMessage(`${capitalized(winner)} is the winner!`)
-}
-
-/* data structure 
-deck: {
-  spades: [
-    2, 3, 4... , J, Q, K, A
-  ],
-  clubs: {
-    ...
   }
-
+  
+function displayResult() {
+  let winner = GAME_STATE.winner?.name ?? null;
+    
+  // prints the winner or a tie
+  winner ? printMessage(`${capitalized(winner)} is the winner!`) : printMessage("It's a tie!")
 }
 
-player: [
-  ['S', '2']
-]
-
-dealer: [
-  ...cards
-]
-
-*/
-
-
-
-
-
-// DEALEAR TURN
-// function dealerTurn() {
-  // hit until:
-  //              1. score is at least 17 but <= 21 (then stay)
-  //              2. score goes above 17 (bust)
-// }
-// PLAYER TURN
-// function playerTurn() {
-//   while (true) {
-//     console.log('Hit or stay');
-//     let answer = readline.question();
-//     if (answer === 'stay' || bust()) break
-//   }
-// }
-
-// if (busted()) {
-//   // end game or ask player to play again
-// } else {
-//   console.log('You chose to stay!')
-// }
-
-// SHUFFLE CARDS BEFORE DEALING
-// function shuffle(arr){
-//   for (let index = arr.length - 1; index > 0; index--) {
-//     let otherIndex = Math.floor(Math.random() * (index + 1));
-//     [arr[otherIndex], arr[index]] = [arr[index], arr[otherIndex]];
-//   }
-
-//   return arr;
-// }
-
-// console.log(shuffle([1, 2, 3, 4, 5]));
-
-
-// FUNCTION FOR RESULT OF GAME
-
-// FUNCTION FOR DISPLAYING GAME RESULT
-
-
-// ALGORITHM
-// create data structures for the deck, player cards and dealer cards
-// start gameloop
-//    shuffle deck
-//    give player two cards, give dealer two cards
-//    playerturn
-//       - player hits or stays
-//         - continues until player busts or stays
-//           - if busts, dealer wins
-//             - display that dealer is the winner
-//           - if stays, dealerturn unless dealer has stayed (in which case compare cards)
-//    dealerturn
-//       - dealer hits until stay (when total is at least 17 but less than 21) or bust
-//         - if stay, back to player
-//         - if bust, player wins
-//           - display that player is the winner
-//    ask user if they want to play again, 
-//        - if yes, shuffle cards and rerun game loop 
-//        - if no, say bye and quit
-
-
+// GAME FLOW
+let sortedDeck = initialiseDeck();
+let shuffledDeck = shuffle(sortedDeck);
+initialiseCardHands(GAME_STATE.players.player.hand, GAME_STATE.players.dealer.hand);
+playerTurn(GAME_STATE.players.player);
