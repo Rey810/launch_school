f02bfb9ea99b6107b7bb84a80d38795d37434abf
reyvdb <reyvdb@gmail.com>
2024-10-04 16:04:56 +0900

    add solutions for #7-12

--- js110/pedac_practice/js101-119_Small_Problems/easy_5.js
+++ js110/pedac_practice/js101-119_Small_Problems/easy_5.js
@@ -370,3 +370,320 @@ multiplicativeAverage(array)
 
 // multiplicativeAverage([3, 5]);                   // "7.500"
 // multiplicativeAverage([2, 5, 7, 11, 13, 17]);    // "28361.667"
+
+
+
+/////////////////////////
+//// Multiply Lists ////
+///////////////////////
+
+/* Problem
+
+- input
+-> two arrays (numbers)
+- output
+-> one array
+
+- rules
+-> returned array contains product of each number pair from two array args
+-> numbers of the same index must be multiplied together
+
+- assumptions
+-> arrays will always be the same length
+-> two arrays will always be provided
+
+- own words
+-> take two arrays of equal lenght, multiply their numbers with one another at each index, return the result of that in one array
+
+----
+Algorithm:
+1. Given two arrays
+2. Multiply numbers of the same index together
+3. Add numbers to new array
+4. Return new array
+
+Main Function:
+multiplyList(arr1, arr2)
+1. Create 'productArray' array2
+2. Iterate over arr1,
+  - at each iteration, multiply number at current index with number in arr2 at current index
+  - add product to 'productArray'
+3. Return 'productArray'
+*/
+
+// function multiplyList(arr1, arr2) {
+//   return arr1.reduce((arr, current, index) => arr.concat(current * arr2[index]), []);
+// }
+
+// console.log(multiplyList([3, 5, 7], [9, 10, 11]));    // [27, 50, 77]
+
+
+
+/////////////////////////
+//// List of Digits ////
+///////////////////////
+
+/* Problem:
+
+- input
+-> number
+- output 
+-> array (number split into separate digits)
+
+- rules
+-> number will always be a positive integer
+
+- assumptions
+-> number will always be provided
+
+---- 
+Algorithm:
+1. Split the numbers
+2. Add each number to an array
+3. Return the array
+
+Main function:
+digitList(number)
+1. Coerce to a string
+2. split string into 'digits' array
+3. iterate over array and coerce back into number
+4. return 'digits' array
+*/
+
+// const digitList = (num) => [...String(num)].map(Number);
+
+// console.log(digitList(12345));       // [1, 2, 3, 4, 5]
+// console.log(digitList(7));           // [7]
+// console.log(digitList(375290));      // [3, 7, 5, 2, 9, 0]
+// console.log(digitList(444));         // [4, 4, 4]
+
+
+
+/////////////////////////
+//// How Many ////
+///////////////////////
+
+/*
+Problem:
+
+- input
+-> an array of strings 
+- output
+-> strings (line by line showing # of occurrences of unique string)
+
+- rules
+-> words are case sensitive (eg. "Cat" !== 'cat')
+
+- assumptions
+-> the arg array will only contain strings
+-> the array will not be empty
+-> an argument will always be provided
+
+- own words
+-> given an array of strings, determine how many times a unique string occurs and return that as a string
+
+---- 
+Algorithm:
+1. Given an array
+2. Count number of items
+3. Return string containg the item and the # of occurrences
+
+Main Function:
+countOccurrences(array)
+1. create object "uniqueCounts"
+2. loop over argument array
+  - if string exists in "uniqueCounts"
+    -> increment count for that string in "uniqueCounts"
+  - else 
+    -> create new key-value pair for string in "uniqueCounts" and set value to 1
+3. for each key-value pair in "uniqueCounts"
+  - log the string "${key} => value"
+  */
+
+//   function countOccurrences(array) {
+//     let uniqueCounts = {};
+
+//     for(let i = 0; i < array.length; i += 1) {
+//       uniqueCounts[array[i]] = uniqueCounts[array[i]] + 1 || 1;
+//     }
+
+//     for (let key in uniqueCounts) console.log(`${key} => ${uniqueCounts[key]}`);
+//   }
+
+//   let vehicles = ['car', 'car', 'truck', 'car', 'SUV', 'truck',
+//     'motorcycle', 'suv', 'motorcycle', 'car', 'truck'];
+
+// countOccurrences(vehicles);
+
+// console output -- your output sequence may be different
+// car => 4
+// truck => 3
+// SUV => 1
+// motorcycle => 2
+// suv => 1
+
+
+
+
+/////////////////////////
+//// Array Average ////
+///////////////////////
+
+/*
+Problem:
+
+- input 
+-> an array of numbers
+- output
+-> a number (average)
+
+- rules
+-> array will always contain positive integers
+-> round down the result to closest whole number
+-> one array will always be provided as an argument
+
+- own words
+-> given an array of numbers, calculate the average number
+
+----
+Algorithm:
+1. Loop over the numbers
+2. Keep track of the sum of numbers
+3. Divide by array length
+4. Return result
+
+Main function:
+average(array)
+1. Create variable "sum"
+2. Loop over array arg
+  - Add number to "sum"
+3. Divide "sum" by array length
+4. Return result of that division
+*/
+
+// function average(array) {
+//   return Math.floor(array.reduce((sum, current) => sum + current) / array.length);
+// }
+
+// console.log(average([1, 5, 87, 45, 8, 8]));       // 25
+// console.log(average([9, 47, 23, 95, 16, 52]));    // 40
+
+
+
+//////////////////////////////////
+//// After Midnight (Part 1) ////
+////////////////////////////////
+
+/* 
+Problem:
+
+- input
+-> integer (positive or negative)
+- output
+-> string (hh:mm)
+
+- rules
+-> any integer can be given as an argument
+-> negative integer is time before midnight
+-> positive integer is time after midnight
+-> ignore any time complications (eg. Daylight Savings)
+-> Don't use Date class methods
+
+- assumptions
+-> integer will always be provided
+
+- own words
+-> given an integer, return the time as a string in "hh:mm" format
+
+--- 
+Algorithm:
+1. If integer argument less than 0, subtract integer from midnight
+2. If integer argument greater than 0, add integer to midnight
+3. If integer argument is zero, do nothing to midnight
+4. Return new time
+
+Main Function:
+timeOfDay(integer)
+1. create MINUTES_IN_DAY variable and set to 60 * 24 (number of minutes)
+2. If integer < 0, minues integer from "MINUTES_IN_DAY"
+3. If integer > 0, add integer to "MINUTES_IN_DAY"
+4. Convert "MINUTES_IN_DAY" to "hh:mm" format
+5. Return result
+*/
+// const MINUTES_PER_HOUR = 60;
+// const HOURS_PER_DAY = 24
+// const MINUTES_IN_DAY = MINUTES_PER_HOUR * HOURS_PER_DAY;
+
+// function leadingZero(number) {
+//   return number < 10 ? `0${number}` : String(number);
+// }
+
+// function formatTime(hours, minutes) {
+//   return `${leadingZero(hours)}:${leadingZero(minutes)}`;
+// }
+
+// function timeOfDay(deltaMinutes) {
+
+
+//   if (deltaMinutes < 0) {
+//     deltaMinutes = (deltaMinutes % MINUTES_IN_DAY) + MINUTES_IN_DAY;
+//   } else {
+//     deltaMinutes = deltaMinutes % MINUTES_IN_DAY;
+//   }
+
+//   let hours = Math.floor(deltaMinutes / MINUTES_PER_HOUR);
+//   let minutes = deltaMinutes % MINUTES_PER_HOUR;
+
+//   return formatTime(hours, minutes);
+
+// }
+
+// console.log(timeOfDay(0) === "00:00");
+// console.log(timeOfDay(-3) === "23:57");
+// console.log(timeOfDay(35) === "00:35");
+// console.log(timeOfDay(-1437) === "00:03");
+// console.log(timeOfDay(3000) === "02:00");
+// console.log(timeOfDay(800) === "13:20");
+// console.log(timeOfDay(-4231) === "01:29");
+
+
+
+//////////////////////////////////
+//// After Midnight (Part 2) ////
+////////////////////////////////
+
+/* 
+Problem:
+
+- input 
+-> string (hh:mm format)
+- output
+-> number (midnights before/after midnight)
+
+- rules
+-> time is given in 24h format
+-> both functions should return a value in the range of 0..1439
+*/
+
+// function afterMidnight(string) {
+//   let numArray = string.split(":").map(el => Number(el));
+//   let hours = numArray[0];
+//   let minutes = numArray[1];
+
+//   return ((hours * 60) + minutes) % 1440;
+// }
+
+// function beforeMidnight(string) {
+//   let numArray = string.split(":").map(el => Number(el));
+//   let hours = numArray[0];
+//   let minutes = numArray[1];
+
+//   return (1440 - ((hours * 60) + minutes)) % 1440;
+// }
+
+// console.log(afterMidnight("00:00") === 0);
+// console.log(beforeMidnight("00:00") === 0);
+// console.log(afterMidnight("12:34") === 754);
+// console.log(beforeMidnight("12:34") === 686);
+// console.log(afterMidnight("24:00") === 0);
+// console.log(beforeMidnight("24:00") === 0);
