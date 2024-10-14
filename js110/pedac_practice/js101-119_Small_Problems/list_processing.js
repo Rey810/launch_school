e7ac711cedc15339b9be8e025292556e8b0ddaf4
reyvdb <reyvdb@gmail.com>
2024-10-14 15:39:45 +0900

    add solution for #10

--- js110/pedac_practice/js101-119_Small_Problems/list_processing.js
+++ js110/pedac_practice/js101-119_Small_Problems/list_processing.js
@@ -386,3 +386,24 @@ function transactionsFor(id, transactionsArray) {
 // [ { id: 101, movement: "in",  quantity:  5 },
 //   { id: 101, movement: "in",  quantity: 12 },
 //   { id: 101, movement: "out", quantity: 18 }, ]
+
+function isItemAvailable(id, transactionsArray) {
+  return transactionsFor(id, transactionsArray).reduce((quantity, curr) => {
+    return curr.movement === "in" ? quantity + curr.quantity : quantity - curr.quantity
+  }, 0) > 0
+}
+
+// let transactions = [ { id: 101, movement: 'in',  quantity:  5 },
+//   { id: 105, movement: 'in',  quantity: 10 },
+//   { id: 102, movement: 'out', quantity: 17 },
+//   { id: 101, movement: 'in',  quantity: 12 },
+//   { id: 103, movement: 'out', quantity: 20 },
+//   { id: 102, movement: 'out', quantity: 15 },
+//   { id: 105, movement: 'in',  quantity: 25 },
+//   { id: 101, movement: 'out', quantity: 18 },
+//   { id: 102, movement: 'in',  quantity: 22 },
+//   { id: 103, movement: 'out', quantity: 15 }, ];
+
+// console.log(isItemAvailable(101, transactions));     // false
+// console.log(isItemAvailable(103, transactions));     // false
+// console.log(isItemAvailable(105, transactions));     // true
