06b3c5eaded5be5c4b64a7ab43237d514a381e60
reyvdb <reyvdb@gmail.com>
2024-10-17 09:27:19 +0900

    add solution for #10

--- js110/pedac_practice/js101-119_Small_Problems/string_and_text_processing.js
+++ js110/pedac_practice/js101-119_Small_Problems/string_and_text_processing.js
@@ -315,36 +315,59 @@ searchWord(targetWord, stringOfWords)
 4. Return count
 */
 
-function searchWord(word, stringOfWords) {
-  let count = 0;
-  let targetWord = word.toLowerCase();
-  let string = stringOfWords.toLowerCase();
+// function searchWord(word, stringOfWords) {
+//   let count = 0;
+//   let targetWord = word.toLowerCase();
+//   let string = stringOfWords.toLowerCase();
 
-  string.split(" ").forEach(currentWord => {
-    let firstIndex = currentWord.indexOf(targetWord[0]);
-    let targetWordFound = false;
-    let targetWordIndex = 0;
+//   string.split(" ").forEach(currentWord => {
+//     let firstIndex = currentWord.indexOf(targetWord[0]);
+//     let targetWordFound = false;
+//     let targetWordIndex = 0;
 
-    if (firstIndex >= 0) {
-      // iterates over every letter
-      for (let idx = firstIndex; idx < (firstIndex + targetWord.length); idx++) {
-        if (targetWord[targetWordIndex] !== currentWord[idx]) {
-          targetWordIndex += 1;
-          targetWordFound = false;
-          continue;
-        }
+//     if (firstIndex >= 0) {
+//       // iterates over every letter
+//       for (let idx = firstIndex; idx < (firstIndex + targetWord.length); idx++) {
+//         if (targetWord[targetWordIndex] !== currentWord[idx]) {
+//           targetWordIndex += 1;
+//           targetWordFound = false;
+//           continue;
+//         }
 
-        targetWordIndex += 1;
-        targetWordFound = true;
-      }
-    }
+//         targetWordIndex += 1;
+//         targetWordFound = true;
+//       }
+//     }
 
-    if (targetWordFound) count += 1;
-  });
+//     if (targetWordFound) count += 1;
+//   });
 
-  return count;
-}
+//   return count;
+// }
 
 // const text = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?';
 
 // console.log(searchWord('sed', text));      // 3
+
+
+
+
+/*
+SEARCH WORD (PART TWO)
+
+- input
+-> word and string of multiple words
+- output
+-> string with found words highlighted and uppercased
+
+*/
+
+function searchWord(wordArg, str) {
+  return str.split(" ").map(word => word.toLowerCase() === wordArg.toLowerCase() ? `**${word.toUpperCase()}**` : word).join(' ')
+}
+
+const text = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Sed quis autem vel est, iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur blasedbla?';
+
+// console.log(searchWord('sed', text));
+// // returns
+// "**SED** ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, **SED** quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, **SED** quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? **SED** quis autem vel est, iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur blasedbla?"
