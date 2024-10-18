7a4a55e83101d59ee49996518e34ec6223bdfc2c
reyvdb <reyvdb@gmail.com>
2024-10-18 16:41:21 +0900

    add solutions for #1-2 for med2

--- js110/pedac_practice/js101-119_Small_Problems/medium_1.js
+++ js110/pedac_practice/js101-119_Small_Problems/medium_1.js
@@ -118,11 +118,11 @@ Main function:
 5. Return result
 */
 
-// function rotateRightmostDigits(number, count) {
-//   let numArray = [...String(number)];
-//   let removed = numArray.splice(numArray.length - count, 1);
-//   return Number(numArray.concat(removed[0]).join(''));
-// }
+function rotateRightmostDigits(number, count) {
+  let numArray = [...String(number)];
+  let removed = numArray.splice(numArray.length - count, 1);
+  return Number(numArray.concat(removed[0]).join(''));
+}
 
 // console.log(rotateRightmostDigits(735291, 1));      // 735291
 // console.log(rotateRightmostDigits(735291, 2));      // 735219
@@ -130,3 +130,236 @@ Main function:
 // console.log(rotateRightmostDigits(735291, 4));      // 732915
 // console.log(rotateRightmostDigits(735291, 5));      // 752913
 // console.log(rotateRightmostDigits(735291, 6));      // 352917
+
+
+
+
+/*
+ROTATION (PART 3)
+
+- input
+-> number
+- output
+-> maximally rotated number
+
+735291 (0 fixed)
+352917 (1 fixed)
+329175 (2 fixed)
+321759 (3 fixed)
+321597 (4 fixed)
+321579 (maximum rotation of the number)
+
+----
+Algorithm
+- Rotate a number maximally
+-- continue rotating number until all numbers have been rotated
+
+maxRotation(number)
+- create 'length' and set to length of number
+- create 'currNum' and set to number arg
+- create a for loop, start at length, decrement
+  -- rotateRightMostDigit(currentNum, idxOfForLoop)
+  --- reassign 'currNum' to return value of function
+- return 'currNum'
+*/
+
+function maxRotation(number) {
+  let length = String(number).split('').length;
+
+  for (let rotationPos = length; rotationPos > 1; rotationPos--) {
+    number = rotateRightmostDigits(number, rotationPos);
+  }
+
+  return number;
+}
+
+// maxRotation(735291);          // 321579
+// maxRotation(3);               // 3
+// maxRotation(35);              // 53
+// maxRotation(105);             // 15 -- the leading zero gets dropped
+// maxRotation(8703529146);      // 7321609845
+
+
+
+
+
+/*
+STACK MACHINE INTERPRETATION
+
+- input
+-> string of instructions
+- output
+-> integer
+
+- rules
+-> commands
+--> n : Place a value, n, in the register. Do not modify the stack.
+--> PUSH : Push the register value onto the stack. Leave the value in the register.
+--> ADD : Pop a value from the stack and add it to the register value, storing the result in the register.
+--> SUB : Pop a value from the stack and subtract it from the register value, storing the result in the register.
+--> MULT : Pop a value from the stack and multiply it by the register value, storing the result in the register.
+--> DIV : Pop a value from the stack and divide the register value by the popped stack value, storing the integer result back in the register.
+--> REMAINDER : Pop a value from the stack and divide the register value by the popped stack value, storing the --> integer remainder of the division back in the register.
+--> POP : Remove the topmost item from the stack and place it in the register.
+--> PRINT : Print the register value.
+-> register aka current value
+-> operation that requires 2 values, pops the topmost, operates on that and the current value (register) and stores value in register
+
+---- 
+ALGORITHM
+- Iterate over a string
+- Perform actions based on the string values
+- Print the result
+
+Main function:
+minilang(str)
+- create "stack" and set to []
+- create "register" and set to 0
+- split str into 'strArr'
+- loop over 'strArr'
+-- remove element from 'strArr'
+--- if element is a number, reassign "register" to number
+--- else if element is "PRINT", print 'register' 
+--- else run other actions
+---- switch statement (element)
+----- case: PUSH
+------ push 'register' onto 'stack'
+----- case: ADD
+------ pop value from 'stack', add it to 'register' and reassign 'register' to result
+----- case: SUB
+------ pop value from 'stack', subtract from 'register' and reassign 'register' to result
+----- case: MULT
+------ pop value from 'stack', multiply by 'register' and reassign 'register' to result
+----- case DIV
+----- pop value from 'stack', divide 'register' by popped value, reassign 'register' to result
+----- case REMAINDER
+----- pop value from 'stack', divide 'register' by popped value, reassign 'register' to remainder
+----- case POP
+------ remove the topmost item from stack and ressign 'register' to it
+*/
+
+function minilang(str) {
+  const stack = [];
+  let register = 0;
+  const strArr = str.split(' ');
+
+  strArr.map(el => {
+    switch (el) {
+      case 'PUSH':
+        Number(stack.push(register));
+        break;
+      case 'ADD':
+        register += stack.pop()
+        break;
+      case 'SUB':
+        register -= stack.pop()
+        break;
+      case 'MULT':
+        register *= stack.pop()
+        break;
+      case 'DIV':
+        register = parseInt(register / stack.pop())
+        break;
+      case 'REMAINDER':
+        register = register % stack.pop()
+        break;
+      case 'POP':
+        register = stack.pop()
+        break;
+      case 'PRINT':
+        console.log(register);
+        break;
+      default:
+        register = Number(el);
+    }
+  });
+}
+
+
+// minilang('PRINT');
+// // 0
+
+// minilang('5 PUSH 3 MULT PRINT');
+// // 15
+
+// minilang('5 PRINT PUSH 3 PRINT ADD PRINT');
+// // // 5
+// // // 3
+// // // 8
+
+// minilang('5 PUSH POP PRINT');
+// // 5
+
+// minilang('3 PUSH 4 PUSH 5 PUSH PRINT ADD PRINT POP PRINT ADD PRINT');
+// // 5
+// // 10
+// // 4
+// // 7
+
+// minilang('3 PUSH PUSH 7 DIV MULT PRINT');
+// // 6
+
+// minilang('4 PUSH PUSH 7 REMAINDER MULT PRINT');
+// // 12
+
+// minilang('-3 PUSH 5 SUB PRINT');
+// // 8
+
+// minilang('6 PUSH');
+// (nothing is printed because the `program` argument has no `PRINT` commands)
+
+
+
+
+
+
+// console.log(letterPercentages('abCdef 123')); 
+// //{ lowercase: "50.00", uppercase: "10.00", neither: "40.00" }
+
+// console.log(letterPercentages('AbCd +Ef'));
+// // { lowercase: "37.50", uppercase: "37.50", neither: "25.00" }
+
+// console.log(letterPercentages('123'));
+// { lowercase: "0.00", uppercase: "0.00", neither: "100.00" }
+
+
+
+
+/*
+WORD DIGIT
+
+- input
+-> string
+- output
+-> string -> number-word strings changed to integers
+
+- rules 
+-> numbers will be between 0 and 9
+
+----
+Algorithm
+- Convert a string with worded numbers into one with integer numbers
+
+Main function
+wordToDigit(str)
+- create 'numbers' array containing number-words from "zero" to "nine"
+- iterate over 'numbers'
+-- replace currEl in string with current index
+- return string
+
+*/
+
+function wordToDigit(str) {
+  const numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+
+  numbers.forEach((el, idx) => {
+    str = str.replaceAll(el, idx);
+  });
+
+  return str;
+
+}
+
+
+console.log(wordToDigit('Please call me at five five five one two three four. Thanks.'));
+// "Please call me at 5 5 5 1 2 3 4. Thanks."
---
+++ js110/pedac_practice/js101-119_Small_Problems/medium_2.js
@@ -0,0 +1,183 @@
+/*
+Lettercase Percentage Ratio
+Write a function that takes a string and returns an object containing the following three properties:
+
+the percentage of characters in the string that are lowercase letters
+the percentage of characters that are uppercase letters
+the percentage of characters that are neither
+You may assume that the string will always contain at least one character.
+
+letterPercentages('abCdef 123'); 
+// { lowercase: "50.00", uppercase: "10.00", neither: "40.00" }
+
+letterPercentages('AbCd +Ef');
+// { lowercase: "37.50", uppercase: "37.50", neither: "25.00" }
+
+letterPercentages('123');
+// { lowercase: "0.00", uppercase: "0.00", neither: "100.00" }
+
+- input
+-> string
+- output
+-> object 
+
+- rules
+-> object will contain 
+--> % lowercase charaters 
+--> % UPPERCASE charaters 
+--> % non-alphabetic charaters 
+-> always 1 character
+-> default return: { lowercase: "0.00", uppercase: "0.00", neither: "0.00"}
+
+----
+Algorithm
+- Find %'s of lowercase, uppercase, non-alpha characters
+
+Main function:
+letterPercentages(string)
+- create "letterCount" obj and set to { lowercase: 0, uppercase: 0, neither: 0 }
+- split into array
+-- determine character
+-- increment object key 
+- create "totalChars" and set to string length
+- create "letterPercent" obj
+-- set "lowercase" key to a percentage 
+-- repeat for uppercase and neither
+-- return letterPercent
+
+Helper function:
+determineCharacter(char)
+- if alphabetic 
+-- if equal to uppercase version, return "uppercase"
+-- if equal to lowercase version, return "lowercase"
+- else non-alphabetic (use "a-z" conditional)
+-- return "neither"
+
+calculatePercent(typeCount, strLength)
+- Calculate percentage
+- Fix to two decimal points
+- Stringify
+- Return result
+*/
+
+function letterPercentages(str) {
+  const letterCount = { lowercase: 0, uppercase: 0, neither: 0 };
+  let totalChar = str.length;
+
+  str.split("").forEach(char => {
+    let type = determineCharacter(char)
+    letterCount[type] += 1;
+  })
+  
+  const letterPercent = {
+    lowercase: calculatePercent(letterCount['lowercase'], totalChar),
+    uppercase: calculatePercent(letterCount['uppercase'], totalChar),
+    neither: calculatePercent(letterCount['neither'], totalChar),
+  }
+
+  return letterPercent;
+}
+
+function determineCharacter(char) {
+  if ((char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z')) {
+    return char === char.toUpperCase() ? "uppercase" : "lowercase";
+  } else {
+    return "neither";
+  }
+}
+
+function calculatePercent(typeCount, strLength) {
+  return String(((typeCount / strLength) * 100).toFixed(2));
+}
+
+/*
+TRIANGLE SIDES
+
+- input
+-> 3 numbers (triangle side lengths)
+- output
+-> string (triangle type)
+
+- rules
+-> valid triangle:
+--> sum of 2 short sides > longest side
+--> every side length > 0
+
+-> isosceles triangle
+--> Two sides are of equal length, while the third is different.
+-> equilateral triangle
+--> All three sides are of equal length
+-> equilateral triangle
+--> All three sides are of different lengths
+
+- return "equilateral" "isosceles" "scalene" or "invalid"
+
+- own words
+-> given the lengths of a triangle, determine if and then what kind of triangle is 
+
+---- 
+Algorithm
+- Deterimine if inputs make a triangle 
+- Deterimine the type of triangle it is
+
+Main function:
+triangle(A, B, C)
+- return "invalid" if not valid (helper)
+- if isEquilateral
+-- return "Equilateral"
+- else if isIsosceles
+-- return "Isosceles"
+-- else return "Scalene"
+
+
+Helper function
+isValid(A, B, C)
+- create 'sides' array and add A, B, C to it
+- if A, B, or C is <= 0, return "invalid"
+- calculate largest number and set to 'largestSide'
+- remove largest number from 'sides'
+- sum elements in 'sides' and set to 'shortSummed'
+- if 'shortSummed' is less than 'largestSide' return "invalid"
+
+Helper function:
+isEquilateral(A, B, C)
+- if A is equal to B AND B is equal to C
+-- return true
+- else
+-- return false
+*/
+
+function triangle(A, B, C) {
+  if (!isValid(A, B, C)) return "invalid";
+
+  if (isEquilateral(A, B, C)) return "Equilateral";
+  else if (isIsosceles(A, B, C)) return "Isosceles";
+  else return "Scalene";
+}
+
+
+function isValid(A, B, C) {
+  const sides = [A, B, C];
+  if (sides.some(side => side <= 0)) return false;
+
+  const largestSize = sides.splice(sides.indexOf(Math.max(...sides)), 1)[0];
+  const shortSummed = sides.reduce((sum, curr) => sum + curr);
+  if (shortSummed < largestSize) return false;
+
+  return true;
+}
+
+function isEquilateral(A, B, C) {
+  return A === B && B === C;
+}
+
+function isIsosceles(A, B, C) {
+  return ( A === B && A !== C) ||  (A !== B && A === C) || (B === C && A !== C)
+}
+
+
+console.log(triangle(3, 3, 3));        // "equilateral"
+console.log(triangle(3, 3, 1.5));      // "isosceles"
+console.log(triangle(3, 4, 5));        // "scalene"
+console.log(triangle(0, 3, 3));        // "invalid"
+console.log(triangle(3, 1, 1));        // "invalid"
