24671d87946a4cf76b9583167b0137bc93c1b0cd
reyvdb <reyvdb@gmail.com>
2024-10-25 16:35:57 +0900

    add solutions #6-9

--- js110/pedac_practice/interview_practice_problems.js
+++ js110/pedac_practice/interview_practice_problems.js
@@ -127,12 +127,12 @@ function minimumSum(array) {
 Problem 3
 Create a function that takes a string argument and returns a copy of the string with every second character in every third word converted to uppercase. Other characters should remain the same.
 */
-let original = 'Lorem Ipsum is simply dummy text of the printing world';
-let expected = 'Lorem Ipsum iS simply dummy tExT of the pRiNtInG world';
+// let original = 'Lorem Ipsum is simply dummy text of the printing world';
+// let expected = 'Lorem Ipsum iS simply dummy tExT of the pRiNtInG world';
 // p(toWeirdCase(original) === expected);
 
-original = 'It is a long established fact that a reader will be distracted';
-expected = 'It is a long established fAcT that a rEaDeR will be dIsTrAcTeD';
+// original = 'It is a long established fact that a reader will be distracted';
+// expected = 'It is a long established fAcT that a rEaDeR will be dIsTrAcTeD';
 // p(toWeirdCase(original) === expected);
 
 // p(toWeirdCase('aaA bB c') === 'aaA bB c');
@@ -382,8 +382,257 @@ function minimumSum(array) {
   return minSum;
 }
 
-p(minimumSum([1, 2, 3, 4]) === null);
-p(minimumSum([1, 2, 3, 4, 5, -5]) === 9);
-p(minimumSum([1, 2, 3, 4, 5, 6]) === 15);
-p(minimumSum([55, 2, 6, 5, 1, 2, 9, 3, 5, 100]) === 16);
-p(minimumSum([-1, -5, -3, 0, -1, 2, -4]) === -10);
+// p(minimumSum([1, 2, 3, 4]) === null);
+// p(minimumSum([1, 2, 3, 4, 5, -5]) === 9);
+// p(minimumSum([1, 2, 3, 4, 5, 6]) === 15);
+// p(minimumSum([55, 2, 6, 5, 1, 2, 9, 3, 5, 100]) === 16);
+// p(minimumSum([-1, -5, -3, 0, -1, 2, -4]) === -10);
+
+
+
+
+/*
+Problem 6
+Create a function that takes a string argument and returns a hash in which the keys represent the lowercase letters in the string, and the values represent how often the corresponding letter occurs in the string.
+
+In: string
+Output: object
+Rules:
+  - Only lowercase letters are counted
+
+----
+Algorithm
+- Count the lowercase letters 
+- Return an object of these letters and counts
+- Return an empty object if there are no lowercase letters
+
+1. create letterCounts object
+2. Iterate over the string
+  - If the letter exists in letterCounts
+    -- Increment this letters count
+  - Otherwise if its a lowercase letter
+    -- Add the letter to letterCounts and set to 1
+3. Return letterCounts
+*/
+
+function countLetters(string) {
+  const letterCounts = {};
+
+  [...string].forEach(char => {
+    if (letterCounts[char]) letterCounts[char] += 1;
+    else if (char >= 'a' && char <= 'z') letterCounts[char]  = 1;
+  })
+
+  return letterCounts;
+}
+
+const objeq = function(obj1, obj2) {
+  let keys1 = Object.keys(obj1);
+  let keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (let key of keys1) {
+    if (! keys2.includes(key)) {
+      return false;
+    } else if (obj1[key] !== obj2[key]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// let expected = {'w': 1, 'o': 2, 'e': 3, 'b': 1, 'g': 1, 'n': 1};
+// p(objeq(countLetters('woebegone'), expected));
+
+// expected = {'l': 1, 'o': 1, 'w': 1, 'e': 4, 'r': 2,
+//             'c': 2, 'a': 2, 's': 2, 'u': 1, 'p': 2};
+// p(objeq(countLetters('lowercase/uppercase'), expected));
+
+// expected = {'u': 1, 'o': 1, 'i': 1, 's': 1};
+// p(objeq(countLetters('W. E. B. Du Bois'), expected));
+
+// p(objeq(countLetters('x'), {'x': 1}));
+// p(objeq(countLetters(''), {}));
+// p(objeq(countLetters('!!!'), {}));
+
+
+
+
+/*
+Problem 7
+Create a function that takes an array of integers as an argument and returns the number of identical pairs of integers in that array. For instance, the number of identical pairs in [1, 2, 3, 2, 1] is 2: there are two occurrences each of both 2 and 1.
+
+If the array is empty or contains exactly one value, return 0.
+
+If a certain number occurs more than twice, count each complete pair once. For instance, for [1, 1, 1, 1] and [2, 2, 2, 2, 2], the function should return 2. The first array contains two complete pairs while the second has an extra 2 that isn't part of the other two pairs.
+
+In: array (numbers)
+Out: number
+Rules:
+  - identical pairs: 2 of the same number
+  - eg [1, 1, 1, 1] -> 2 identical pairs
+
+----
+Algorithm
+- Count the occurrences of numbers
+- Count the identical pairs
+
+1. Get the number counts (see numCounts) and set to "numCountsObj"
+2. create "identicalPairsCount" and set to 0
+3. Iterate over "numCounts"
+  - Count pairs and increment "identicalPairsCount" by count
+4. Return "identicalPairsCount"
+
+function numCounts(arr)
+1. Create "counts" obj
+2. Iterate over numbers
+    - If the number exists in obj
+      -- Increment it by 1
+    - Otherwise create a key-value of num: 1
+3. Return counts
+*/
+
+function pairs(arr) {
+  const numCountsObj = numCounts(arr);
+  let identicalPairsCount = 0;
+  
+  for (let numKey in numCountsObj) {
+    identicalPairsCount += Math.floor(numCountsObj[numKey] / 2);
+  }
+
+  return identicalPairsCount;
+}
+
+function numCounts(arr) {
+  const counts = {};
+
+  arr.forEach(num => {
+    if (counts[num]) counts[num] += 1;
+    else counts[num] = 1;
+  })
+
+  return counts;
+}
+
+// p(pairs([3, 1, 4, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7]) === 3);
+// p(pairs([2, 7, 1, 8, 2, 8, 1, 8, 2, 8, 4]) === 4);
+// p(pairs([]) === 0);
+// p(pairs([23]) === 0);
+// p(pairs([997, 997]) === 1);
+// p(pairs([32, 32, 32]) === 1);
+// p(pairs([7, 7, 7, 7, 7, 7, 7]) === 3);
+
+
+
+
+/*
+Problem 8
+
+Create a function that takes a non-empty string as an argument. The string consists entirely of lowercase alphabetic characters. The function should return the length of the longest vowel substring. The vowels of interest are "a", "e", "i", "o", and "u".
+
+In: string ('a-z')
+Out: number (longest vowel substring)
+Rules:
+  - Return 0 if no vowel substring is found
+
+---------
+Algorithm
+- Find the longest vowel substring
+- Return the length of that substring
+
+1. Create 'subStrLength' and set it to 0
+2. Create a 'currentSubstring'
+2. Iterate over the string
+  - Check if the letter is a vowel
+    -- Append it to 'currentSubstring' 
+    -- If 'currentSubstring' is now bigger than 'subStringLength
+      --- Set 'subStringLength' to 'currentSubstring' length
+  - Otherwise set 'currentSubstring' to 0
+3. Return 'subStrLength'
+
+Helper function
+isVowel(letter)
+1. Create a 'vowels' array
+2. Return true if letter is found in 'vowels' array
+3. Otherwise return false
+*/
+
+function longestVowelSubstring(str) {
+  let subStrLength = 0;
+  let currentSubstring = '';
+
+  [...str].forEach(letter => {
+
+    if (isVowel(letter)) {
+      currentSubstring += letter;
+      if (currentSubstring.length > subStrLength) subStrLength = currentSubstring.length;
+    } else currentSubstring = '';
+  })
+
+  return subStrLength;
+}
+
+function isVowel(letter) {
+  const vowels = ["a", "e", "i", "o", "u"];
+
+  return vowels.includes(letter);
+}
+
+// p(longestVowelSubstring('cwm') === 0);
+// p(longestVowelSubstring('many') === 1);
+// p(longestVowelSubstring('launchschoolstudents') === 2);
+// p(longestVowelSubstring('eau') === 3);
+// p(longestVowelSubstring('beauteous') === 3);
+// p(longestVowelSubstring('sequoia') === 4);
+// p(longestVowelSubstring('miaoued') === 5);
+
+
+
+
+/*
+Problem 9
+Create a function that takes two string arguments and returns the number of times that the second string occurs in the first string. Note that overlapping strings don't count: 'babab' contains 1 instance of 'bab', not 2.
+
+You may assume that the second argument is never an empty string.
+
+In: 2 strings (source, target)
+Out: number (count of target in source
+Rules:
+  - overlapping strings don't count (i.e. 'babab' has 1 'bab')
+
+-----
+Algorithm
+- Find all occurrences of target string
+- Count the occurrences of target string
+
+*/
+
+
+function countSubstrings(source, target) {
+  let counter = 0;
+
+  for (let i = 0; i <= source.length - target.length; i++) {
+    let sourceSubString = source.slice(i, i + target.length);
+
+    if (sourceSubString === target) {
+      counter += 1;
+      i += sourceSubString.length - 1;
+    }
+  }
+
+  return counter;
+}
+
+// p(countSubstrings('babab', 'bab') === 1);
+// p(countSubstrings('babab', 'ba') === 2);
+// p(countSubstrings('babab', 'b') === 3);
+// p(countSubstrings('babab', 'x') === 0);
+// p(countSubstrings('babab', 'x') === 0);
+// p(countSubstrings('', 'x') === 0);
+// p(countSubstrings('bbbaabbbbaab', 'baab') === 2);
+// p(countSubstrings('bbbaabbbbaab', 'bbaab') === 2);
+// p(countSubstrings('bbbaabbbbaabb', 'bbbaabb') === 1);
