60ad010b98abbbc46b95d728f32f058e609418c7
reyvdb <reyvdb@gmail.com>
2025-03-14 08:56:11 +0900

    add rest of the solutions

--- js120/lesson_5/exercises/ooBasics_classes.js
+++ js120/lesson_5/exercises/ooBasics_classes.js
@@ -11,13 +11,40 @@ class Cat {
     this.name = name;
   }
 
-  greet() {
+  rename(name) {
+    this.name = name;
+  }
+
+  personalGreeting() {
     console.log(`Hello! My name is ${this.name}!`);
+  }
+
+  static genericGreeting() {
+    console.log("Hello! I'm a cat!");
   }
 }
 
 // Create an instance
 const kitty = new Cat('Sophie');
+kitty.rename('Chloe');
+console.log(kitty.name); // Chloe
+
+//Hello, Chloe
+
 
 // What are you
 kitty.greet();
+
+// Default Person
+class Person {
+  constructor(name = "John Doe") {
+    this.name = name;
+  }
+}
+
+let person1 = new Person();
+let person2 = new Person("Pepe");
+
+console.log(person1.name); // John Doe
+console.log(person2.name); // Pepe
+
